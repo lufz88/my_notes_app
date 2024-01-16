5ec8fd3336508c78a1823d966b8a7821ff702b8a
Lucas Frazzetta <lucasfrazzetta@gmail.com>
2024-01-16 01:19:47 -0300

    Updating script

--- setup.sh
+++ setup.sh
@@ -13,17 +13,10 @@ cd backend
 npm install
 
 # Create and start the database in Docker
-docker run --name my-postgres-db -e POSTGRES_PASSWORD=admin123 -p 5432:5432 -d postgres:latest
+docker-compose up -d postgres
 
 # Wait for a few seconds to ensure the database is ready
 sleep 5
-
-# Create the 'lucas' role in the PostgreSQL server with necessary permissions
-docker exec -it my-postgres-db psql -U postgres -c "CREATE ROLE lucas WITH LOGIN PASSWORD 'admin123'; ALTER USER lucas CREATEDB;"
-
-
-# Create the my_notes_app database
-docker exec -it my-postgres-db psql -U postgres -c "CREATE DATABASE my_notes_app;"
 
 # Start the backend in development mode
 npm run dev &
